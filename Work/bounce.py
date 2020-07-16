d3f620102b87e6610bd94206f68892f8cedeb7cc
S M Arifuzzaman <ariff@stansall.com>
2020-07-16 20:01:23 -0230

    Assignment 4 completed!

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounces = 0
+distance = height
+while bounces < 10:
+    height *= (3/5)
+    print(bounces + 1, round(height, 4))    
+    bounces += 1
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,21 @@
 # pcost.py
 #
 # Exercise 1.27
+import sys
+
+def portfolio_cost(filename):
+    totalcost = 0.0
+    with open(filename, 'rt') as file:
+        next(file)
+        for line in file:
+            #print(line.split(',')[1])        
+            totalcost += int(line.split(',')[1]) * float(line.split(',')[2])
+    return totalcost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost: ', cost)
