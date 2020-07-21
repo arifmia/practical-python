d5d610b180759770d41f1a6673dc769475a836cf
S M Arifuzzaman <ariff@stansall.com>
2020-07-21 01:29:11 -0230

    Assignment 5 Done & Commit to git

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,20 +2,32 @@
 #
 # Exercise 1.27
 import sys
+import csv
 
 def portfolio_cost(filename):
     totalcost = 0.0
     with open(filename, 'rt') as file:
-        next(file)
-        for line in file:
-            #print(line.split(',')[1])        
-            totalcost += int(line.split(',')[1]) * float(line.split(',')[2])
+        rows = csv.reader(file)
+        headers = next(rows)
+        #for line in file:
+        for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
+            print(record)
+            #totalcost += int(line.split(',')[1]) * float(line.split(',')[2])
+            try:
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                totalcost += nshares * price
+            # This catches errors in int() and float() conversions above
+            except ValueError:
+                print(f'Row {rowno}: Bad row: {row}')
     return totalcost
 
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
-    filename = 'data/portfolio.csv'
+    #filename = 'data/portfolio.csv'
+    filename = 'data/portfoliodate.csv'
 
 cost = portfolio_cost(filename)
 print('Total cost: ', cost)
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,69 @@
 # report.py
 #
 # Exercise 2.4
+import sys
+import csv
+from pprint import pprint
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'rt') as file:
+        rows = csv.reader(file)
+        headers = next(rows)
+        for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
+            #print(record)
+            try:
+                stock = {
+                    'name'   : record['name'],
+                    'shares' : int(record['shares']),
+                    'price'   : float(record['price'])
+                }
+                portfolio.append(stock)
+            except ValueError:
+                print(f'Row {rowno}: Bad row: {row}')
+    return portfolio
+
+def read_prices(filename):
+    prices = {}
+    with open(filename, 'rt') as file:
+        rows = csv.reader(file)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+
+    return prices
+
+def create_report(portfolio, prices):
+    rows = []
+    for stock in portfolio:
+        price = prices[stock['name']]
+        change = price - stock['price']
+        summary = (stock['name'], stock['shares'], price, change)
+        rows.append(summary)
+    return rows
+
+if len(sys.argv) == 3:
+    filePortfolio = sys.argv[1]
+    filePrices = sys.argv[2]
+else:
+    filePortfolio = 'data/portfoliodate.csv'
+    filePrices = 'data/prices.csv'
+
+# Call Read data files function here
+portfolio = read_portfolio(filePortfolio)
+prices = read_prices(filePrices)
+#pprint(portfolio)
+#pprint(prices)
+# Create report
+report = create_report(portfolio, prices)
+#report output
+headers = ('Name', 'Shares', 'Price', 'Change')
+#print(headers)
+print('%10s %10s %10s %10s' % headers)
+print(('-' * 10 + ' ') * len(headers))
+for row in report:
+    #print("%10s %10d %10.2f %10.2f" % row)
+    print('%10s %10d %10.2f %10.2f' %row)
