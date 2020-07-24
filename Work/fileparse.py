207e8d1fd7dbbce42fa1c7f987c292105ec3cbcb
S M Arifuzzaman <ariff@stansall.com>
2020-07-24 01:11:43 -0230

    Assignment 6: Commit Exercise 3.18

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -5,64 +5,46 @@ import sys
 import csv
 import gzip
 
+# Exercise 3.18: Fixing existing functions
 def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
-#def parse_csv(filename, select=None, types=None, has_headers=False):
     '''
     Parse a CSV file into a list of records
-    '''   
-    #with open(filename) as f:
-    with gzip.open(filename, 'rt') as f:
-        rows = csv.reader(f, delimiter=delimiter)
-       
-        # Read the file headers
-        if has_headers:
-            headers = next(rows)
+    '''       
+    rows = csv.reader(filename, delimiter=delimiter)
+    
+    # Read the file headers
+    if has_headers:
+        headers = next(rows)
 
-        # If a column selector was given, find indices of the specified columns.
-        # Also narrow the set of headers used for resulting dictionaries
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        else:
-            indices = []
+    # If a column selector was given, find indices of the specified columns.
+    # Also narrow the set of headers used for resulting dictionaries
+    if select:
+        indices = [headers.index(colname) for colname in select]
+        headers = select
+    else:
+        indices = []
 
-        records = []
-        for rowno, row in enumerate(rows, 1):
-            if not row:    # Skip rows with no data
+    records = []
+    for rowno, row in enumerate(rows, 1):
+        if not row:    # Skip rows with no data
+            continue
+        # Filter the row if specific columns were selected
+        if indices:
+            row = [ row[index] for index in indices ]
+        if types:
+            try:
+                row = [func(val) for func, val in zip(types, row) ]
+            except ValueError as e:
+                if not silence_errors:
+                    print(f"Row {rowno}: Couldn't convert {row}")
+                    print(f"Row {rowno}: Reason {e}")
                 continue
-            # Filter the row if specific columns were selected
-            if indices:
-                row = [ row[index] for index in indices ]
-            if types:
-                try:
-                    row = [func(val) for func, val in zip(types, row) ]
-                except ValueError as e:
-                    if not silence_errors:
-                        print(f"Row {rowno}: Couldn't convert {row}")
-                        print(f"Row {rowno}: Reason {e}")
-                    continue
 
-            # Make a dictionary
-            if has_headers:
-                record = dict(zip(headers, row))
-                records.append(record)
-            else:
-                records.append(row)
+        # Make a dictionary
+        if has_headers:
+            record = dict(zip(headers, row))
+            records.append(record)
+        else:
+            records.append(row)
 
     return records
-
-if len(sys.argv) == 2:
-    csvfilepath = sys.argv[1]
-else:
-    csvfilepath = 'data/portfolio.csv.gz'
-    #csvfilepath = 'data/missing.csv'
-
-# Exercise 3.17: From filenames to file-like objects
-#try:
-#read_csv = parse_csv(csvfilepath, types=[str, int, float], silence_errors=True)
-read_csv = parse_csv(csvfilepath, types=[str, int, float])
-#except (IOError,LookupError,RuntimeError, ValueError) as e:    
-    #raise
-#read_csv = parse_csv(csvfilepath, types=[str, int, float], has_headers=True, delimiter=' ')
-
-print(read_csv)
--- Work/report.py
+++ Work/report.py
@@ -7,10 +7,12 @@ from pprint import pprint
 import fileparse
 
 def read_portfolio(filename):
-    return fileparse.parse_csv(filename, select=['name','shares','price'], types=[str,int,float])
+    with open(filename) as lines:
+        return fileparse.parse_csv(lines, select=['name','shares','price'], types=[str,int,float])
 
 def read_prices(filename):
-    return dict(fileparse.parse_csv(filename,types=[str,float], has_headers=False))
+    with open(filename) as lines:
+        return dict(fileparse.parse_csv(lines,types=[str,float], has_headers=False))
 
 def create_report(portfolio, prices):
     rows = []
@@ -22,12 +24,10 @@ def create_report(portfolio, prices):
     return rows
 
 def print_report(report):
-    headers = ('Name', 'Shares', 'Price', 'Change')
-    #print(headers)
+    headers = ('Name', 'Shares', 'Price', 'Change')    
     print('%10s %10s %10s %10s' % headers)
     print(('-' * 10 + ' ') * len(headers))
-    for row in report:
-        #print("%10s %10d %10.2f %10.2f" % row)
+    for row in report:        
         print('%10s %10d %10.2f %10.2f' %row)
 
 def portfolio_report(filePortfolio, filePrices):
@@ -39,21 +39,14 @@ def portfolio_report(filePortfolio, filePrices):
     # Call custome report print function
     print_report(report)
 
-# Exercise 3.16: Making Scripts
+# Exercise 3.18: Fixing existing functions
 # Main function
 def main(argv):
     # Parse command line args, environment, etc.   
-    if len(sys.argv) == 3:
-        #filePortfolio = sys.argv[1]
-        #filePrices = sys.argv[2]
+    if len(sys.argv) == 3:        
         portfolio_report(sys.argv[1], sys.argv[2])
     else:
-        raise SystemExit('Usage: %s portfoliofile pricefile' % argv[0])
-        #filePortfolio = 'data/portfolio.csv'
-        #filePrices = 'data/prices.csv'
-    
-# Call this single function to produce the report
-#portfolio_report(filePortfolio, filePrices)
+        raise SystemExit('Usage: %s portfoliofile pricefile' % argv[0])        
 
 if __name__ == '__main__':    
     main(sys.argv)
